46de9c7f0b143589eb78f2464bbdea654da0353e
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2022-07-06 13:25:04 -0500

    rutas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  url_api:'https://localhost:7177/api/',
-  url_api_integra:'http://localhost:63048/api/',
-  url_portal:'http://localhost:4200/'
+  url_api:'https://api-portalweb.bsginstitute.com/api/',
+  url_api_integra:'https://integrav4-servicios.bsginstitute.com/api/',
+  url_portal:'https://angular-portal.bsginstitute.com/'
 };
 
 /*
